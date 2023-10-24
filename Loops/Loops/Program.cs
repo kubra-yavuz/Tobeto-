540bf9728779c4c064e67c13bdd45a11f81196f2
Kubra Yavuz <nurkacm@gmail.com>
2023-10-25 01:57:59 +0300

    C# Dersleri 30: For Döngüsü

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,17 @@
+﻿NewMethod();
+
+static void NewMethod()
+{
+    // for (int i = 1; i <= 100; i++) = birer arttır.
+    // for (int i = 1; i <= 100; i+=2) = tek sayıları yazdır.
+    //for (int i = 2; i <= 100; i+=2) = çift sayıları yazdır.
+    for (int i = 100; i >= 0; i=i-2) 
+    {
+        Console.WriteLine(i);
+    }
+    Console.WriteLine("Finished!");
+    Console.ReadLine();
+
+
+}
+
