1036e0b463acce099624cec99e95311e11d1e0f7
Kubra Yavuz <nurkacm@gmail.com>
2023-10-24 22:43:00 +0300

    C# Dersleri 21: Neden Metot Kullanıyoruz?

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,16 @@
+﻿NewMethod();
+
+static void NewMethod()
+{
+    Add();
+    Add();
+    Add();
+    Add();
+
+    Console.ReadLine();
+}
+
+static void Add()
+{
+    Console.WriteLine("Added!!!");
+}
