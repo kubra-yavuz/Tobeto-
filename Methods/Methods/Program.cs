e5018ed490ba992215773d5ec19184ede2b3e3f1
Kubra Yavuz <nurkacm@gmail.com>
2023-10-25 00:20:50 +0300

    C# Dersleri 24: Challenge : Ref Keyword İle Çalışmak

--- Methods/Methods/Program.cs
+++ Methods/Methods/Program.cs
@@ -1,24 +1,35 @@
-﻿NewMethod();
+﻿//NewMethod();
 
-static void NewMethod()
-{
-    Add();
-    Add();
-    Add();
-    Add();
-    var result = Add2(20);
 
-    Console.WriteLine(result);
+//static void NewMethod()
+//{
+//    Add();
+//    Add();
+//    Add();
+//    Add();
+//    var result = Add2();
+
+
+    int number1 = 20;
+    int number2 = 100;
+    var result2 = Add3(ref number1, number2);
+    Console.WriteLine(result2);
+    Console.WriteLine(number1);
     Console.ReadLine();
-}
 
 static void Add()
 {
     Console.WriteLine("Added!!!");
 }
 
-static int Add2(int number1,int number2=30)
+static int Add2(int number1=20, int number2=30)
 {
     var result = number1 + number2;
     return result;
+}
+
+static int Add3(ref int number1, int number2)
+{
+    number1 = 30;
+    return number1 + number2; 
 }
