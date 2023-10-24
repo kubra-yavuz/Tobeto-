645c48e36aed4c019e107e37b7e1e3d2f58eb53a
Kubra Yavuz <nurkacm@gmail.com>
2023-10-24 16:04:14 +0300

    C# Dersleri 11: Double Veri Tipi

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ static void NewMethod()
     //Value Types
     //Console.WriteLine("Hello, World!");
 
-
+    double number5 = 10.4;
     char character = 'A';
     bool condition = false; 
     byte number4 = 255;
@@ -16,6 +16,7 @@ static void NewMethod()
     Console.WriteLine("Number2 is {0}", number2);
     Console.WriteLine("Number3 is {0}", number3);
     Console.WriteLine("Number4 is {0}", number4);
+    Console.WriteLine("Number5 is {0}", number5);
     Console.WriteLine("Character is : {0}", (int)character);
 
     Console.ReadLine();
