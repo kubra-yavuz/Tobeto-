f0f0b6b6b73fc16522ef901430537072e6fa4f51
Kubra Yavuz <nurkacm@gmail.com>
2023-10-25 23:42:06 +0300

    C# Dersleri 39: Encapsulation

--- Classes/Classes/Customer.cs
+++ Classes/Classes/Customer.cs
@@ -2,9 +2,18 @@
 namespace Classes
 {
 	class Customer
-	{
+	{ 
+
+		//Property
 		public int Id { get; set; }
-		public string FirstName { get; set; }
+
+		private string _firstName;
+		public string FirstName
+		{
+			get { return "Mr." + _firstName; }
+			set { _firstName = value; }
+
+		}
 		public string LastName { get; set; }
 		public string City { get; set; }
 		
