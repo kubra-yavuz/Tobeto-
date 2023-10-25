b72ef660abb570f22b2a50a1aa55836e8572af31
Kubra Yavuz <nurkacm@gmail.com>
2023-10-25 04:01:32 +0300

    C# Dersleri 36: String Metodlarıyla Çalışmak

--- Strings/Strings/Program.cs
+++ Strings/Strings/Program.cs
@@ -2,6 +2,26 @@
 {
     private static void Main(string[] args)
     {
+        //Intro();
+
+        string sentence = "My name is Engin Demiroğ";
+        var result = sentence.Length; //metnin kaç karakterden oluştuğunu verir.
+        var result2 = sentence.Clone(); //aynısını alıp yeni referans oluşturur.
+        sentence = "My name is Derin Demiroğ";
+
+        bool result3 = sentence.EndsWith("ğ"); // metin "ğ" ile bitiyor mu?
+        bool result4 = sentence.StartsWith("My name"); // my name ile başlıyor mu
+
+        var result5 = sentence.IndexOf("name"); // name ifadesini arar.
+        var result6 = sentence.IndexOf(" "); // boşluğu arar. ilk boşlukta durur.
+        var result7 = sentence.LastIndexOf(" "); //karakteri sondan aramaya başlar.
+
+          Console.WriteLine(result7);
+        Console.ReadLine();
+    }
+
+    private static void Intro()
+    {
         string city = "Ankara";
         //Console.WriteLine(city[0]);
 
@@ -15,6 +35,5 @@
         //Console.WriteLine(result);
 
         Console.WriteLine(String.Format("{0} {1}", city, city2));
-        Console.ReadLine();
     }
 }
